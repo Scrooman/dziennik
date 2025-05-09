b77eda4a7d4151a3fdc0f06f4406e099d9815d6e
Scrooman <kubcwiek@gmail.com>
2025-05-09 07:42:39 +0200

    1

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2KAhjjOYxD0Vbwu4ct4m6sQgaBfMEPHg",
+    authDomain: "dziennik-9623f.firebaseapp.com",
+    databaseURL: "https://dziennik-9623f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "dziennik-9623f",
+    storageBucket: "dziennik-9623f.firebasestorage.app",
+    messagingSenderId: "768677977424",
+    appId: "1:768677977424:web:49cbcf461802629502a055",
+    measurementId: "G-Q5R93ST4PX"
+};
